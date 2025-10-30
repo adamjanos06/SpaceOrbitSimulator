f45ffd135d51c6f161763b8d8116f7e8c6fd305a
AdamJanos <adamjancsi01@gmail.com>
2025-10-30 12:29:42 +0100

    First user inputs
    Customizable details of the rocket

--- SpaceOrbitSimulator/Simulator/Simulator.cpp
+++ SpaceOrbitSimulator/Simulator/Simulator.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "\n*-*-*-*-* ASTRA SIMULATOR *-*-*-*-*\n";
+    std::cout << "Name your Rocket (eg. Apollo11): ";
+    std::string name;
+    getline(std::cin, name);
+    std::cout << "Name your Mission (eg. cargo, satellite, orbit): ";
+    std::string mission;
+    getline(std::cin, mission);
+    std::cout << "Pick your rocket's material (1 for stainless steel, 2 for titanium): ";
+    int materialNum;
+    std::cin >> materialNum;
+    std::string material;
+    switch (materialNum)
+    {
+        case 1:
+            material = "stainless steel";
+            break;
+        case 2:
+            material = "titanium";
+        default:
+            std::cout << "Wrong number";
+    }
+    std::cout << "Enter your stage count (eg. 2-3): ";
+    int stageCount;
+    std::cin >> stageCount;
+    
+    std::cout << "\n\nYour Rocket's details\n";
+    std::cout << name;
+    std::cout << "\nMission: " << mission;
+    std::cout << "\nMaterial: " << material;
+    std::cout << "\nStages: " << stageCount;
+    
+    return 0;
 }
